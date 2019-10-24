2393e5df4e41ba40964382e4c3e78110e74f330e
amir shariff <amirshariff@msn.com>
2019-10-24 03:26:40 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,6 +15,12 @@ end
 
 introduction_with_language("John","React")
 
+def introduction_with_language_optional(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language("John","React")
+
 
 
 
